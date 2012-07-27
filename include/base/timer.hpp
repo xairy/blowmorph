e4ea42777aa4a88bf23b9f7c317a791d46f2c252
Andrey Konovalov <adech.fo@gmail.com>
2012-07-27 21:04:44 +0400

    Timer is fixed

--- include/base/timer.hpp
+++ include/base/timer.hpp
@@ -3,30 +3,63 @@
 
 // WARNING: Timer is NOT thread safe.
 
-#include <ctime>
+#ifdef WIN32
+# include <ctime>
+#else
+# include "sys/time.h"
+#endif
 
+#include "base/macros.hpp"
 #include "base/pstdint.hpp"
 
 namespace bm {
 
+#ifdef WIN32
+
 class Timer {
 public:
-  Timer();
-  ~Timer();
+  Timer() {
+    _start = clock();
+  }
+  ~Timer() { }
 
   // Returns elapsed time in ms since timer's creation.
-  uint32_t GetTime() const;
-
-  // Returns elapsed time since the start of the program.
-  static uint32_t GetAbsoluteTime();
-
-  // Sleeps for 'timeout' ms.
-  static void Sleep(uint32_t timeout);
+  uint32_t GetTime() const {
+    uint64_t clock_diff = static_cast<uint64_t>(clock() - _start);
+    uint32_t result = static_cast<uint32_t>(clock_diff * 1000 / CLOCKS_PER_SEC);
+    return result;
+  }
 
 private:
-  clock_t _clock_start;
+  clock_t _start;
 };
+
+#else
+
+class Timer {
+public:
+  Timer() {
+    int rv = gettimeofday(&_start, NULL);
+    CHECK(rv == 0);
+  }
+
+  uint32_t GetTime() const {
+    timeval current;
+    int rv = gettimeofday(&current, NULL);
+    long seconds = current.tv_sec - _start.tv_sec;
+    long useconds = current.tv_usec - _start.tv_usec;
+    long time = seconds * 1000 + useconds / 1000;
+    CHECK(time >= 0);
+    return static_cast<uint32_t>(time);
+  }
+
+private:
+  timeval _start;
+};
+
+#endif
 
 } // namespace bm
 
 #endif //BLOWMORPH_BASE_TIMER_HPP_
+
