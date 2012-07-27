e433d1db587472f80aba70ab206b1fbb777408e5
Andrey Konovalov <adech.fo@gmail.com>
2012-07-27 21:17:00 +0400

    Timer is finally fixed

--- include/base/timer.hpp
+++ include/base/timer.hpp
@@ -3,6 +3,9 @@
 
 // WARNING: Timer is NOT thread safe.
 
+#include <cstdio>
+#include <cstdlib>
+
 #ifdef WIN32
 # include <ctime>
 #else
@@ -18,17 +21,10 @@ namespace bm {
 
 class Timer {
 public:
-  Timer() {
-    _start = clock();
-  }
-  ~Timer() { }
+  Timer();
 
   // Returns elapsed time in ms since timer's creation.
-  uint32_t GetTime() const {
-    uint64_t clock_diff = static_cast<uint64_t>(clock() - _start);
-    uint32_t result = static_cast<uint32_t>(clock_diff * 1000 / CLOCKS_PER_SEC);
-    return result;
-  }
+  uint32_t GetTime() const;
 
 private:
   clock_t _start;
@@ -38,20 +34,10 @@ private:
 
 class Timer {
 public:
-  Timer() {
-    int rv = gettimeofday(&_start, NULL);
-    CHECK(rv == 0);
-  }
+  Timer();
 
-  uint32_t GetTime() const {
-    timeval current;
-    int rv = gettimeofday(&current, NULL);
-    long seconds = current.tv_sec - _start.tv_sec;
-    long useconds = current.tv_usec - _start.tv_usec;
-    long time = seconds * 1000 + useconds / 1000;
-    CHECK(time >= 0);
-    return static_cast<uint32_t>(time);
-  }
+  // Returns elapsed time in ms since timer's creation.
+  uint32_t GetTime() const;
 
 private:
   timeval _start;
