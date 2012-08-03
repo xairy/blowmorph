f4a133d611a64e0fdabc512a59481279dc9ebaec
Alexander Konovalov <alex.knvl@gmail.com>
2012-08-03 21:53:06 +0400

    Cleaned up the code in TextWriter

--- src/client/text_writer.hpp
+++ src/client/text_writer.hpp
@@ -19,33 +19,15 @@ public:
   ~TextWriter();
 
   // Run it after GL initialization
-  // You can use {Hawaii_Killer.ttf, tahoma.ttf, verdana.ttf} as file_name
   bool InitFont(std::string file_name, int pixel_size);
 
   // Run it before ending
-  void Destroy();  
+  void Destroy();
   
   // This method will print out text at window coordinates X, Y, using the font 
   // specified at initialization. The current modelview matrix will also be 
   // applied to the text.
-  //void PrintText(float x, float y, const char *text, ...);
   void PrintText(glm::vec4 color, float x, float y, const char *text, ...);
-  
-  // Set default printing string, used before Render(float, float) and Render().
-  //void SetPrintingString(std::string printing_string);
-
-  // Draw default printing string from point (x, y). You need run 
-  // SetPrintingString(string) one time before it.
-  //void Render(float x, float y);
-
-  // Set default ccordinates of place, where default string will be drawn, 
-  // used before Render().
-  //void SetCoordinates(float x, float y);
-
-  // Draw default printing string from default point. You need run 
-  // SetCoordinates(float, float) and SetPrintingString(string) one time before
-  // it.
-  //void Render();
 
 private:
   enum state {
@@ -57,10 +39,6 @@ private:
   
   struct FontData;
   FontData* _our_font;
-
-  //std::string _printing_string;
-  //float _x, _y;
-  //glm::vec4 _color;
 };
 
 }; // namespace bm
