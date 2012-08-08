42d77a7990b85099f8c551fb3988e59dd8262360
Andrey Konovalov <adech.fo@gmail.com>
2012-08-08 14:32:42 +0400

    entity.*pp split into different files

---
+++ src/server/dummy.cpp
@@ -0,0 +1,117 @@
+#include "dummy.hpp"
+
+#include <memory>
+#include <string>
+
+#include <base/error.hpp>
+#include <base/macros.hpp>
+#include <base/protocol.hpp>
+#include <base/pstdint.hpp>
+
+#include "vector.hpp"
+#include "shape.hpp"
+#include "world_manager.hpp"
+
+namespace bm {
+
+Dummy* Dummy::Create(
+  WorldManager* world_manager,
+  uint32_t id,
+  const Vector2& position,
+  float radius,
+  float speed,
+  uint32_t time
+) {
+  std::auto_ptr<Dummy> dummy(new Dummy(world_manager, id));
+  if(dummy.get() == NULL) {
+    Error::Set(Error::TYPE_MEMORY);
+    return NULL;
+  }
+  std::auto_ptr<Circle> shape(new Circle(position, radius));
+  if(shape.get() == NULL) {
+    Error::Set(Error::TYPE_MEMORY);
+    return NULL;
+  }
+
+  dummy->_shape = shape.release();
+  dummy->_speed = speed;
+  dummy->_meat = NULL;
+  dummy->_last_update = time;
+  dummy->_prev_position = position;
+
+  return dummy.release();
+}
+
+Dummy::~Dummy() { }
+
+std::string Dummy::GetType() {
+  return "Dummy";
+}
+bool Dummy::IsStatic() {
+  return false;
+}
+
+void Dummy::Update(uint32_t time) {
+  _prev_position = _shape->GetPosition();
+  if(_meat != NULL) {
+    bm::uint32_t dt = time - _last_update;
+    Vector2 direction = _meat->GetPosition() - GetPosition();
+    direction.Normalize();
+    _shape->Move(direction * _speed * static_cast<float>(dt));
+  }
+  _last_update = time;
+}
+EntitySnapshot Dummy::GetSnapshot(uint32_t time) {
+  EntitySnapshot result;
+  result.type = BM_ENTITY_DUMMY;
+  result.time = time;
+  result.id = _id;
+  result.x = _shape->GetPosition().x;
+  result.y = _shape->GetPosition().y;
+  return result;
+}
+
+void Dummy::OnEntityAppearance(Entity* entity) {
+  if(entity->GetType() == "Player") {
+    if(_meat == NULL) {
+      _meat = entity;
+    } else {
+      float current_distance = (_meat->GetPosition() - GetPosition()).Magnitude();
+      float new_distance = (entity->GetPosition() - GetPosition()).Magnitude();
+      if(new_distance < current_distance) {
+        _meat = entity;
+      }
+    }
+  }
+}
+void Dummy::OnEntityDisappearance(Entity* entity) {
+  if(_meat == entity) {
+    _meat = NULL;
+  }
+}
+
+void Dummy::SetPosition(const Vector2& position) {
+  _prev_position = position;
+  _shape->SetPosition(position);
+}
+
+bool Dummy::Collide(Entity* entity) {
+  return entity->Collide(this);
+}
+
+bool Dummy::Collide(Player* other) {
+  return Entity::Collide(other, this);
+}
+bool Dummy::Collide(Dummy* other) {
+  return Entity::Collide(other, this);
+}
+bool Dummy::Collide(Bullet* other) {
+  return Entity::Collide(this, other);
+}
+bool Dummy::Collide(Wall* other) {
+  return Entity::Collide(other, this);
+}
+
+Dummy::Dummy(WorldManager* world_manager, uint32_t id) : Entity(world_manager, id) { }
+
+} // namespace bm
---
+++ src/server/dummy.hpp
@@ -0,0 +1,61 @@
+#ifndef BLOWMORPH_SERVER_DUMMY_HPP_
+#define BLOWMORPH_SERVER_DUMMY_HPP_
+
+#include <string>
+
+#include <base/macros.hpp>
+#include <base/protocol.hpp>
+#include <base/pstdint.hpp>
+
+#include "entity.hpp"
+#include "vector.hpp"
+
+namespace bm {
+
+class Dummy : public Entity {
+  friend class Entity;
+
+public:
+  static Dummy* Create(
+    WorldManager* world_manager,
+    uint32_t id,
+    const Vector2& position,
+    float radius,
+    float speed,
+    uint32_t time
+  );
+  virtual ~Dummy();
+
+  virtual std::string GetType();
+  virtual bool IsStatic();
+
+  virtual void Update(uint32_t time);
+  virtual EntitySnapshot GetSnapshot(uint32_t time);
+
+  virtual void OnEntityAppearance(Entity* entity);
+  virtual void OnEntityDisappearance(Entity* entity);
+
+  virtual void SetPosition(const Vector2& position);
+
+  // Double dispatch. Collision detection.
+
+  virtual bool Collide(Entity* entity);
+
+  virtual bool Collide(Player* other);
+  virtual bool Collide(Dummy* other);
+  virtual bool Collide(Bullet* other);
+  virtual bool Collide(Wall* other);
+
+protected:
+  DISALLOW_COPY_AND_ASSIGN(Dummy);
+  Dummy(WorldManager* world_manager, uint32_t id);
+
+  float _speed;
+  Entity* _meat;
+  uint32_t _last_update;
+  Vector2 _prev_position;
+};
+
+} // namespace bm
+
+#endif // BLOWMORPH_SERVER_DUMMY_HPP_
