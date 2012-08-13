a8bb969c348ff0a51531356e50581b60979aff54
Alexander Konovalov <alex.knvl@gmail.com>
2012-08-13 15:01:07 +0400

    Added Canvas (basic 2d rendering)

---
+++ src/client/canvas.hpp
@@ -0,0 +1,31 @@
+#ifndef BLOWMORPH_CLIENT_CANVAS_HPP_
+#define BLOWMORPH_CLIENT_CANVAS_HPP_
+
+#define _USE_MATH_DEFINES
+#include <glm/glm.hpp>
+
+namespace bm {
+
+class Canvas {
+public:
+  enum CoordinateType {
+    Normalized,
+    Pixels,
+    NormalizedFlipped,
+    PixelsFlipped
+  };
+  
+  bool Init();
+
+  void SetCoordinateType(CoordinateType ct);
+  
+  void DrawRect(const glm::vec4& clr, const glm::vec2& pos, const glm::vec2& size);
+  void FillRect(const glm::vec4& clr, const glm::vec2& pos, const glm::vec2& size);
+
+private:
+  
+};
+
+}; // namespace bm
+
+#endif//BLOWMORPH_CLIENT_CANVAS_HPP_
