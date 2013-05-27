00bc3ee570019a3ae7112dec9616c63e26eaf55e
Alexander Konovalov <alex.knvl@gmail.com>
2013-05-28 00:59:42 +0400

    Client compiles
    --HG--
    branch : issue#36

---
+++ src/client/sys.hpp
@@ -0,0 +1,13 @@
+#ifndef BLOWMORPH_CLIENT_SYS_HPP_
+#define BLOWMORPH_CLIENT_SYS_HPP_
+
+#include <stdint.h>
+
+namespace sys {
+
+/// Returns current timestamp in seconds.
+double Timestamp();
+
+} // namespace sys
+
+#endif  // BLOWMORPH_CLIENT_SYS_HPP_
