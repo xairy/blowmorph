ec5d914b2f8f242eccedcb6125931e82c8b72044
Andrey Konovalov <adech.fo@gmail.com>
2013-09-29 20:55:44 +0400

    Fixed codestyle in src/base/ctrlc.h

--- src/base/ctrlc.h
+++ src/base/ctrlc.h
@@ -1,9 +1,11 @@
-#ifndef CTRLC_H_
-#define CTRLC_H_
+// Copyright (c) 2013 Blowmorph Team
 
-#include "base.h"
+#ifndef SRC_BASE_CTRLC_H_
+#define SRC_BASE_CTRLC_H_
+
+#include <base/base.h>
 
 // FIXME[23.11.2012 alex]: Not thread safe.
 BM_BASE_DECL void SetCtrlCHandler(void (*handler)());
 
-#endif // CTRLC_H_
+#endif  // SRC_BASE_CTRLC_H_
