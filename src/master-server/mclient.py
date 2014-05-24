89f096b3f0eaced92cb8e5e25460b521d7c1a219
Sergey Voronov <rdkl.hrd@gmail.com>
2014-05-24 04:55:18 +0400

    mclient script added

---
+++ src/master-server/mclient.py
@@ -0,0 +1,20 @@
+#-*- coding: utf-8 -*-
+# Copyright (c) 2014 Blowmorph Team
+
+# Game server script: it sends IP and pc name to the master server.
+
+import requests
+import socket
+
+f = open("../../data/client.cfg")
+lines = f.readlines()
+f.close()
+
+for line in lines:
+    if len(line) >= 9 and line[0:9] == "  host = ":
+        host = line[10:-3]
+    if len(line) >= 9 and line[0:9] == "  port = ":
+        port = line[9:-2]
+
+address = "http://" + host + ":" + port
+r = requests.post(address, params = {"name" : socket.gethostname()})
