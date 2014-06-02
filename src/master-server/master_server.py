481a4650a1c02e5dad51f4a5b01e96930bf39042
Andrey Konovalov <adech.fo@gmail.com>
2014-06-02 23:42:04 +0400

    Various fixes

--- src/master-server/master_server.py
+++ src/master-server/master_server.py
@@ -1,52 +1,50 @@
 #-*- coding: utf-8 -*-
 # Copyright (c) 2014 Blowmorph Team
 
-from pylibconfig import Config
 import time
 import BaseHTTPServer
-
-config = Config()
-config.readFile("data/master-server.cfg")
-port = config.value("master-server.port")
-assert port[1] == True
-port = port[0]
+from pylibconfig import Config
 
 game_servers = {}
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_POST(self):
-        args = self.path.decode().split("/?")
-        args = args[1].split("&")
-        args = map(lambda x: tuple(x.split("=")), args)
-        args = dict(args)
-        if not("name" in args.keys()) or not("port" in args.keys()) or not("active" in args.keys()):
-            print "Error: wrong POST data"
+        args = self.path.decode().split("/?")[1].split("&")
+        args = dict(map(lambda x: tuple(x.split("=")), args))
+        if not("name" in args.keys()) or \
+           not("port" in args.keys()) or \
+           not("active" in args.keys()):
+            print "[%s] Error: wrong POST data from %s!" % \
+              (str(time.asctime()), self.client_address[0])
             return
         else:
-            active = args["active"]
-            server_name = args["name"]
-            server_port = args["port"]
-            server_host = self.client_address[0]
-            key = server_host + ":" + server_port
-            if active == "True":
-                game_servers[key] = (server_name, server_host, server_port)
+            gs_active = args["active"]
+            gs_name = args["name"]
+            gs_port = args["port"]
+            gs_host = self.client_address[0]
+            key = gs_host + ":" + gs_port
+            if gs_active == "True":
+                game_servers[key] = (gs_name, gs_host, gs_port)
             elif key in game_servers.keys():
                 game_servers.pop(key, None)
             print game_servers
-            
         self.send_response(200)
         
     def do_GET(self):
         self.wfile.write(str(game_servers))
         
 if __name__ == '__main__':
+    config = Config()
+    config.readFile("data/master-server.cfg")
+    port = config.value("master-server.port")
+    assert port[1] == True
+    port = port[0]
+
     http_server = BaseHTTPServer.HTTPServer(("0.0.0.0", port), RequestHandler)
     print "[%s] Server started on port %d." % (str(time.asctime()), port)
     try:
         http_server.serve_forever()
     except KeyboardInterrupt:
         pass
-    
     http_server.server_close()
-    
     print "[%s] Server stopped." % (str(time.asctime()),)
--- src/master-server/notify.py
+++ src/master-server/notify.py
@@ -8,29 +8,29 @@ import socket
 import sys
 from pylibconfig import Config
 
-if len(sys.argv) != 2:
-    print "Usage: %s <active>" % (sys.argv[0],)
-    exit(0)
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print "Usage: %s <active>" % (sys.argv[0],)
+        exit(0)
+    active = (sys.argv[1] == "true")
 
-active = (sys.argv[1] == "true")
+    config = Config()
+    config.readFile("data/server.cfg")
 
-config = Config()
-config.readFile("data/server.cfg")
+    ms_host = config.value("master-server.host")
+    ms_port = config.value("master-server.port")
+    assert (ms_host[1] == True) and (ms_port[1] == True)
+    ms_host, ms_port = ms_host[0], ms_port[0]
 
-ms_host = config.value("master-server.host")
-ms_port = config.value("master-server.port")
-assert (ms_host[1] == True) and (ms_port[1] == True)
-ms_host, ms_port = ms_host[0], ms_port[0]
+    gs_name = config.value("server.name")
+    gs_port = config.value("server.port")
+    assert (gs_name[1] == True) and (gs_port[1] == True)
+    gs_name, gs_port = gs_name[0], gs_port[0]
 
-gs_name = config.value("server.name")
-gs_port = config.value("server.port")
-assert (gs_name[1] == True) and (gs_port[1] == True)
-gs_name, gs_port = gs_name[0], gs_port[0]
-
-address = "http://" + ms_host + ":" + str(ms_port)
-try:
-    r = requests.post(address, params = {"name" : gs_name, "port": gs_port, "active": active})
-except requests.exceptions.ConnectionError:
-    print "Could not connect to master server %s:%d!" % (ms_host, ms_port)
-else:
-    print "Master server %s:%d notified." % (ms_host, ms_port)
+    address = "http://" + ms_host + ":" + str(ms_port)
+    try:
+        r = requests.post(address, params = {"name" : gs_name, "port": gs_port, "active": active})
+    except requests.exceptions.ConnectionError:
+        print "Could not connect to master server %s:%d!" % (ms_host, ms_port)
+    else:
+        print "Master server %s:%d notified." % (ms_host, ms_port)
