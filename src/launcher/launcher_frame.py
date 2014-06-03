89801a6225856752eb8376811da28db4789c2180
Andrey Konovalov <adech.fo@gmail.com>
2014-06-04 03:27:45 +0400

    Added other settings

--- src/launcher/launcher_frame.py
+++ src/launcher/launcher_frame.py
@@ -10,7 +10,15 @@ from launcher_frame_base import LauncherFrameBase
 def load_config(config, name):
     value = config.value(name.encode("utf8"))
     assert value[1] == True
-    return value[0]
+    value = value[0]
+    if type(value) == str:
+        value = value.decode("utf8")
+    return value
+
+def save_config(config, name, value):
+    if type(value) == unicode:
+        value = value.encode("utf8")
+    config.setValue(name.encode("utf8"), value)
 
 ##############################################################################
 class LauncherFrame(LauncherFrameBase):
@@ -18,33 +26,29 @@ class LauncherFrame(LauncherFrameBase):
     def __init__(self, title="Blowmorph Launcher", size=(330, 400)):
         LauncherFrameBase.__init__(self, None)
         self.selected_address = None
-        self.LoadConfig()
-        self.RetrieveGameServerList()
         self.SetupLayout()
 
     #-------------------------------------------------------------------------
     def SetupLayout(self):
-        self.Bind(wx.EVT_BUTTON, self.OnConnectButtonPressed,
-            self.connect_button)
+        self.LoadConfig()
+
+        # Bind buttons.
         self.Bind(wx.EVT_BUTTON, self.OnQuitButtonPressed,
             self.quit_button)
+        self.Bind(wx.EVT_BUTTON, self.OnSaveButtonPressed,
+            self.save_button)
+        self.Bind(wx.EVT_BUTTON, self.OnConnectButtonPressed,
+            self.connect_button)
+        self.Bind(wx.EVT_BUTTON, self.OnRefreshButtonPressed,
+            self.refresh_button)
 
         column_names = ["Name", "Address"]
         for i in xrange(len(column_names)):
             self.server_list.InsertColumn(i, column_names[i])
             
-        server_list_data = [[self.servers_dict[key][0], key] 
-                            for key in self.servers_dict]
-        
-        # FIXME(xairy): wtf.
-        for item in server_list_data:
-            index = self.server_list.InsertStringItem(sys.maxint, str(item[0]))
-            for p in xrange(len(item) - 1):
-                self.server_list.SetStringItem(index, p + 1, str(item[p + 1]))
-        
-        for i in xrange(len(column_names)):
-            self.server_list.SetColumnWidth(i, self.server_list.GetSize()[0] / 2)
+        self.RefreshGameServerList()
 
+        # Bind list events.
         self.Bind(wx.EVT_LIST_ITEM_SELECTED,
             self.OnItemSelected, self.server_list)
         self.Bind(wx.EVT_LIST_ITEM_DESELECTED,
@@ -65,6 +69,15 @@ class LauncherFrame(LauncherFrameBase):
         # Set fullscreen checkbox.
         self.fullscreen_checkbox.SetValue(self.cfg_fullscreen)
 
+        # Set advanced settings.
+        self.master_server_text.SetValue(
+            "%s:%d" % (self.cfg_ms_host, self.cfg_ms_port))
+        self.connection_timeout_spin.SetValue(self.cfg_connection_timeout)
+        self.sync_timeout_spin.SetValue(self.cfg_sync_timeout)
+        self.max_misposition_spin.SetValue(self.cfg_max_misposition)
+        self.interpolation_offset_spin.SetValue(self.cfg_interpolation_offset)
+        self.tick_rate_spin.SetValue(self.cfg_tick_rate)
+
         self.Layout()
 
     #-------------------------------------------------------------------------
@@ -72,25 +85,21 @@ class LauncherFrame(LauncherFrameBase):
         self.Destroy()
 
     #-------------------------------------------------------------------------
+    def OnSaveButtonPressed(self, event):
+        self.SaveConfig()
+
+    #-------------------------------------------------------------------------
     def OnConnectButtonPressed(self, event):
         if self.selected_address == None:
             return
-
-        address = self.selected_address.split(":")
-        host, port = address[0], int(address[1])
-
-        selection = self.resolution_choice.GetSelection() 
-        resolution = self.resolution_choice.GetString(selection)
-        resolution = resolution.split('(')[0].split('x')
-        width, height = int(resolution[0]), int(resolution[1])
-
-        fullscreen = self.fullscreen_checkbox.GetValue()
-        nickname = self.nickname_text.GetValue()
-
-        self.SaveConfig(host=host, port=port, width=width,
-            height=height, fullscreen=fullscreen, login=nickname)
-
+        self.SaveConfig()
         subprocess.call("./client.sh")
+
+    #-------------------------------------------------------------------------
+    def OnRefreshButtonPressed(self, event):
+        ms_addr = self.master_server_text.GetValue().split(':')
+        self.cfg_ms_host, self.cfg_ms_port = ms_addr[0], int(ms_addr[1])
+        self.RefreshGameServerList()
 
     #-------------------------------------------------------------------------
     def OnItemSelected(self, event):
@@ -121,25 +130,77 @@ class LauncherFrame(LauncherFrameBase):
         self.cfg_ms_host = load_config(self.config, "master-server.host")
         self.cfg_ms_port = load_config(self.config, "master-server.port")
 
+        self.cfg_connection_timeout = \
+            load_config(self.config, "client.connect_timeout")
+        self.cfg_sync_timeout = \
+            load_config(self.config, "client.sync_timeout")
+        self.cfg_max_misposition = \
+            load_config(self.config, "client.max_player_misposition")
+        self.cfg_interpolation_offset = \
+            load_config(self.config, "client.interpolation_offset")
+        self.cfg_tick_rate = \
+            load_config(self.config, "client.tick_rate")
+
     #-----------------------------------------------------------------------------
-    def RetrieveGameServerList(self):
+    def RefreshGameServerList(self):
+        servers = {}
         try:
             address = "http://%s:%d" % (self.cfg_ms_host, self.cfg_ms_port)
             r = requests.get(address)
-            self.servers_dict = ast.literal_eval(r.text)
+            servers = ast.literal_eval(r.text)
         except requests.exceptions.ConnectionError:
             print "Could connect to master server: %s:%d!" % \
               (self.cfg_ms_host, self.cfg_ms_port)
-            self.servers_dict = {}
+
+        self.server_list.DeleteAllItems()
+
+        for key, value in servers.items():
+            name = value["name"]
+            addr = "%s:%d" % (value["host"], value["port"])
+            index = self.server_list.InsertStringItem(sys.maxint, name)
+            self.server_list.SetStringItem(index, 1, addr)
+        
+        for i in xrange(self.server_list.GetColumnCount()):
+            self.server_list.SetColumnWidth(i, self.server_list.GetSize()[0] / 2)
 
     #-------------------------------------------------------------------------
-    def SaveConfig(self, host, port, width, height, fullscreen, login):
-        self.config.setValue("server.host".encode("utf8"), host.encode("utf8"))
-        self.config.setValue("server.port".encode("utf8"), port)
-        self.config.setValue("graphics.width".encode("utf8"), width);
-        self.config.setValue("graphics.height".encode("utf8"), height);
-        self.config.setValue("graphics.fullscreen".encode("utf8"), fullscreen);
-        self.config.setValue("player.login".encode("utf8"), login.encode("utf8"));
+    def SaveConfig(self):
+        if self.selected_address != None:
+            address = self.selected_address.split(":")
+            host, port = address[0], int(address[1])
+        else:
+            host, port = "0.0.0.0", 0
+
+        selection = self.resolution_choice.GetSelection() 
+        resolution = self.resolution_choice.GetString(selection)
+        resolution = resolution.split('(')[0].split('x')
+        width, height = int(resolution[0]), int(resolution[1])
+
+        fullscreen = self.fullscreen_checkbox.GetValue()
+
+        nickname = self.nickname_text.GetValue()
+
+        ms_addr = self.master_server_text.GetValue().split(':')
+        ms_host, ms_port = ms_addr[0], int(ms_addr[1])
+
+        connection_timeout = self.connection_timeout_spin.GetValue()
+        sync_timeout = self.sync_timeout_spin.GetValue()
+        max_misp = float(self.max_misposition_spin.GetValue())
+        inter_offset = self.interpolation_offset_spin.GetValue()
+        tick_rate = self.tick_rate_spin.GetValue()
+
+        save_config(self.config, "server.host", host)
+        save_config(self.config, "server.port", port)
+        save_config(self.config, "graphics.width", width)
+        save_config(self.config, "graphics.height", height)
+        save_config(self.config, "graphics.fullscreen", fullscreen)
+        save_config(self.config, "player.login", nickname)
+        save_config(self.config, "client.connect_timeout", connection_timeout)
+        save_config(self.config, "client.sync_timeout", sync_timeout)
+        save_config(self.config, "client.max_player_misposition", max_misp)
+        save_config(self.config, "client.interpolation_offset", inter_offset)
+        save_config(self.config, "client.tick_rate", tick_rate)
+
         self.config.writeFile("data/client.cfg".encode("utf8"))
     #-------------------------------------------------------------------------
 ##############################################################################
