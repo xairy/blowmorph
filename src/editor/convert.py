52861c69379b3dabccc4beaa4fc646eb2c13a852
Andrey Konovalov <adech.fo@gmail.com>
2015-03-21 01:13:00 +0300

    Add Entity layer to tiled map

--- src/editor/convert.py
+++ src/editor/convert.py
@@ -5,9 +5,10 @@ from __future__ import unicode_literals
 import json
 import sys
 
-def id_to_tile(id, tiled_map):
+def id_to_tile(id, tm):
+  assert id > 0
   last_tileset = None
-  for tileset in tiled_map['tilesets']:
+  for tileset in tm['tilesets']:
     if id >= tileset['firstgid']:
       last_tileset = tileset
       continue
@@ -15,30 +16,50 @@ def id_to_tile(id, tiled_map):
   tile_id = id - last_tileset['firstgid']
   return (tileset_name, tile_id)
 
-def tile_to_sprite(tileset_name, tile_id):
+def terrain_tile_to_sprite(tileset_name, tile_id):
   m = {
     'land': ['land_grass', 'land_concrete', 'land_brick', 'land_wood'],
     'hitech': [None, None, None, 'hitech_plate']
   }
   return m[tileset_name][tile_id]
 
-def get_terrain_layer(tiled_map):
-  for layer in tiled_map['layers']:
-    if layer['name'] == 'Terrain':
+def entity_tile_to_entity(tileset_name, tile_id):
+  m = {
+    'walls': [None, 'morphed_wall', 'unbreakable_wall', 'ordinary_wall'],
+  }
+  return m[tileset_name][tile_id]
+
+def get_layer(tm, layer_name):
+  for layer in tm['layers']:
+    if layer['name'] == layer_name:
       return layer
   assert False
 
-assert len(sys.argv) == 3
+assert len(sys.argv) == 2
 src = sys.argv[1]
-dst = sys.argv[2]
 
-tiled_map = json.loads(open(src).read())
+tm = json.loads(open(src).read())
 
-terrain = []
-for id in get_terrain_layer(tiled_map)['data']:
-  terrain.append(tile_to_sprite(*id_to_tile(id, tiled_map)))
+m = {}
+m['width'] = tm['width']
+m['height'] = tm['height']
+m['block_size'] = float(tm['tilewidth'])
 
-terrain = str(terrain)
-terrain = terrain.replace("u'", '"')
-terrain = terrain.replace("'", '"')
-print terrain
+m['terrain'] = []
+for id in get_layer(tm, 'Terrain')['data']:
+  m['terrain'].append(terrain_tile_to_sprite(*id_to_tile(id, tm)))
+
+m['walls'] = []
+for i, id in enumerate(get_layer(tm, 'Entity')['data']):
+  if id == 0:
+    continue
+  x = i % m['width']
+  y = i / m['width']
+  entity = entity_tile_to_entity(*id_to_tile(id, tm))
+  wall = {'entity': entity, 'x': x, 'y': y}
+  m['walls'].append(wall)
+
+m['spawns'] = [{'x': 10, 'y': 10}]
+m['kits'] = [{'entity': 'health_kit', 'x': 1, 'y': 1}]
+
+print json.dumps(m, indent=4, separators=(',', ': '))
