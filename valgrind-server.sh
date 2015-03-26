fcfaa2ca6404ad33127bcd868a3ef16a819e8819
Andrey Konovalov <adech.fo@gmail.com>
2015-03-26 14:34:11 +0300

    Change valgrind-server script

--- valgrind-server.sh
+++ valgrind-server.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-sigint_handler()
-{
-  python src/master-server/notify.py false
-  exit $?
-}
-
-trap sigint_handler SIGINT
-
-python src/master-server/notify.py true
-
 export LD_LIBRARY_PATH="`pwd`/bin"
 valgrind --leak-check=full ./bin/server
